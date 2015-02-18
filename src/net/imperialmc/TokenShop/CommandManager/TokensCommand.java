ef8a2bd4e02493413eeda7a72314195bd071ca74
millenium200 <moonmasters200@gmail.com>
2015-02-17 23:23:30 -0500

    Update to /tokens

--- src/net/imperialmc/TokenShop/CommandManager/TokensCommand.java
+++ src/net/imperialmc/TokenShop/CommandManager/TokensCommand.java
@@ -1,24 +1,26 @@
 package net.imperialmc.TokenShop.CommandManager;
 
 import net.imperialmc.TokenShop.Main;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 
 public class TokensCommand implements CommandExecutor, Listener {
 
-    private Main plugin;
+  private Main plugin;
 
-    public TokensCommand(Main Main) {
+  public TokensCommand(Main Main) {
 
-        // PLUGIN RUNS BACK TO MAIN CLASS
-        plugin = Main;
+    // PLUGIN RUNS BACK TO MAIN CLASS
+    plugin = Main;
 
-        // COMMAND
-        plugin.getCommand("PUT COMMAND IN HERE").setExecutor(this);
+    // COMMAND
+    plugin.getCommand("PUT COMMAND IN HERE").setExecutor(this);
 
-    }
+  }
 
   @Override
   public boolean onCommand(CommandSender sender, Command cmd, String StringLabel,
@@ -40,25 +42,55 @@ public class TokensCommand implements CommandExecutor, Listener {
         }
 
         Player player = (Player) sender;
-        int playerTokens = TokenManager.getTokens(player.getUniqueId()
-            .toString());
-        sender.sendMessage(announcePrefix + "You have " + playerTokens
-            + "tokens!");
-        sender
-            .sendMessage(announcePrefix
-                + "Use /tshop to use your tokens!, Use /tokens help for more info!");
+        alertPlayerTheirTokens(player);
         return true;
       }
       else if (args.length == 1) {
-        sender.sendMessage(announcePrefix + ChatColor.BOLD 
-            + "Imperial Networks Tokens!");
-        String playerName = args[0];
-        String playerID = UUIDFetcher.getUUIDof(playerName).toString();
-        int playerTokens = TokenManager.getTokens(playerID);
+        if (!args[0].equals("help") {
+          sender.sendMessage(announcePrefix + ChatColor.BOLD 
+              + "Imperial Networks Tokens!");
+          String playerName = args[0];
+          
+          if((sender instanceof Player)) {
+            Player player = (Player) sender;
+            if (player.getName().equals(playerName)) {
+              alertPlayerTheirTokens(player);
+              return true;
+            }
+            if (!player.hasPermission("millenium.tokenshop.tokens.others")) {
+              sender.sendMessage("You don't have enough swag.");
+              return true;
+            }
+          }
+          
+          String playerID = UUIDFetcher.getUUIDof(playerName).toString();
+          int playerTokens = TokenManager.getTokens(playerID);
+          sender.sendMessage(playerID);
+          //DEBUG ^
+          sender.sendMessage(playerName + " has " + playerTokens + " tokens!");
+        }
+        else {
+          sender.sendMessage(announcePrefix + ChatColor.BOLD + "Imperial Networks Tokens!");
+          sender.sendMessage(announcePrefix + "/tokens [playerName]");
+          sender.sendMessage(announcePrefix + "/tokens <give|set|take> <playerName>");
+          sender.sendMessage(announcePrefix + "/tshop");
+          sender.sendMessage(announcePrefix + "Earn tokens through voting, events, and more!");
+        }
       }
 
     }
     return false;
   }
-
+  
+  public static void alertPlayerTheirTokens(Player player) {
+    int playerTokens = TokenManager.getTokens(player.getUniqueId()
+        .toString());
+    sender.sendMessage(announcePrefix + "You have " + playerTokens
+        + "tokens!");
+    sender
+        .sendMessage(announcePrefix
+            + "Use /tshop to use your tokens!, Use /tokens help for more info!");
+  }
+  
+  
 }
