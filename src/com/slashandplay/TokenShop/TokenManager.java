d5e6996098ce4e79588c26999258b89fb10d6f01
millenium200 <moonmasters200@gmail.com>
2015-03-08 01:23:52 -0500

    fixes

--- src/com/slashandplay/TokenShop/TokenManager.java
+++ src/com/slashandplay/TokenShop/TokenManager.java
@@ -9,7 +9,6 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.plugin.Plugin;
 
 public class TokenManager implements Listener {
 
