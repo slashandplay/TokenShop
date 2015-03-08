97ce88f8bfdf472176230649b8d386bb3438fd13
millenium200 <moonmasters200@gmail.com>
2015-03-08 00:44:35 -0500

    Update UUIDFetcher.java

--- src/net/imperialmc/TokenShop/UUIDFetcher.java
+++ src/net/imperialmc/TokenShop/UUIDFetcher.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package net.imperialmc.TokenShop;
+package com.slashandplay.TokenShop;
 
 import com.google.common.collect.ImmutableList;
 import org.json.simple.JSONArray;
